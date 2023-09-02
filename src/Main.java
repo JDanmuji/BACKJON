54a5c345f14ab863c42315844f4fecf16724f805
JDanmuji <danmuji0724@gmail.com>
2023-09-02 23:06:20 +0900

    230902 B_2083 ing

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String info;
 
-        int num = scanner.nextInt();
-        int count = 0;
-        for(int i = 1; i <= num; i++) {
-            count+=i;
+
+        List<String[]> newList = new ArrayList<>();
+
+
+        while (scanner.hasNextLine()) { // 입력이 더 존재하는 동안
+
+            info = scanner.nextLine();
+            if (info.contains("#")) { // 마지막 입력
+                break;
+            }
+
+            String[] lineInfo = info.split(" ");
+            newList.add(lineInfo); // 한 줄씩 입력 받음
+
+
         }
 
-        System.out.println(count);
+        for(Object object : newList) {
+            String element = (String) object;
+            System.out.println(element);
+        }
+
     }
 }
