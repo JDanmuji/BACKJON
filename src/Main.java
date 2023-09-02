8fdb366781b0ae6c7ba7cd50bb554e1989e5e5e2
JDanmuji <danmuji0724@gmail.com>
2023-09-03 03:56:32 +0900

    B_6749, B_2083 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,5 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String info;
 
-
-        List<String[]> newList = new ArrayList<>();
-
-
-        while (scanner.hasNextLine()) { // 입력이 더 존재하는 동안
-
-            info = scanner.nextLine();
-            if (info.contains("#")) { // 마지막 입력
-                break;
-            }
-
-            String[] lineInfo = info.split(" ");
-            newList.add(lineInfo); // 한 줄씩 입력 받음
-
-
-        }
-
-        for(Object object : newList) {
-            String element = (String) object;
-            System.out.println(element);
-        }
-
-    }
 }
