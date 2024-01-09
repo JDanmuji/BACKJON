430aacb4502ca1c434419b0c23079bc05a249772
JDanmuji <danmuji0724@gmail.com>
2024-01-09 17:28:28 +0900

    b_1546 add

--- src/Bronze/B_1546.java
+++ src/Bronze/B_1546.java
@@ -1,13 +1,14 @@
+package Bronze;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.nio.Buffer;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class B_1546 {
 
-    public static void main(String[] args) throws IOException {
+     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int count  = Integer.parseInt(br.readLine());
@@ -34,4 +35,5 @@ public class Main {
 
   
     }
+    
 }
