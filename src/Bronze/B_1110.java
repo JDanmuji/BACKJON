058a449a1b59ceeca73e7c6feddef92180c1917d
JDanmuji <danmuji0724@gmail.com>
2024-01-12 16:47:13 +0900

    B_1110

--- src/Bronze/B_1110.java
+++ src/Bronze/B_1110.java
@@ -1,12 +1,13 @@
+package Bronze;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.nio.Buffer;
 import java.util.*;
-import java.util.stream.Stream;
 
-public class Main {
+
+public class B_1110 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
